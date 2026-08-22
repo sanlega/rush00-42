0e38e8b6c91839c075e83c9e10c11ef6a1c0f0db
Santi Legaristi Royo <salegari@c2r11s6.42madrid.com>
2026-08-22 21:14:18 +0200

    Generador de matrices para 9x9

--- ex00/main.c
+++ ex00/main.c
@@ -6,23 +6,151 @@
 /*   By: cerodrig <cerodrig@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 14:44:41 by cerodrig          #+#    #+#             */
-/*   Updated: 2026/08/22 17:44:26 by cerodrig         ###   ########.fr       */
+/*   Updated: 2026/08/22 21:10:15 by salegari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
 //void	convert_str_to_int(char *str);
 void	rush(char *str);
 
-int	main(int argc, char *argv[])
+int	ft_isspace(char c)
 {
-	if (argc < 2)
-	{
-		write(1, "Pasa la cadena, bro!", 20);
+	if (c == '\t' || c == '\n' || c == '\r'
+		|| c == '\f' || c == '\v' || c == ' ')
 		return (1);
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	res;
+	int	s;
+
+	i = 0;
+	res = 0;
+	s = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			s *= -1;
+		i++;
 	}
-	rush(argv[1]);
-	//convert_str_to_int(argv[1]);
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * s);
+}
+
+int	ft_isnumber(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] < '0' || str [i] > '9')
+			return 0;
+		i++;
+	}
 	return (1);
+}
+
+void	ft_free(int **board, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		free(board[i]);
+		i++;
+	}
+}
+
+int	*ft_conform(char **nb, int size, int row)
+{
+	int	i;
+	int	*table;
+
+	i = 0;
+	table = malloc(size * sizeof(int));
+	if (!table)
+		return (NULL);
+	while (i < size)
+		table[i++] = 0;
+	if (row == 0)
+	{
+		i = 1;
+		while (i < size - 1)
+		{
+			table[i] = ft_atoi(nb[i]);
+			i++;
+		}
+	}
+	else if (row == size - 1)
+	{
+		i = 1;
+		while (i < size - 1)
+		{
+			table[i] = ft_atoi(nb[i + 4]);
+			i++;
+		}
+	}
+	else
+	{
+		table[0] = ft_atoi(nb[row + 8]);
+		table[size - 1] = ft_atoi(nb[row + 12]);
+	}
+	return (table);
+}
+
+int	main(int ac, char *av[])
+{
+	int	i;
+	int	**board;
+	int	 size;
+
+	i = 0;
+	size = (ac - 1) / 4 + 2;
+	if (ac != 17)
+		return (write(1, "Error\n", 6), 0);
+	board = malloc(size * sizeof(int *));
+	if (!board)
+		return (write(1, "Error\n", 6), 0);
+	while (i < size)
+	{
+		board[i] = ft_conform(av, size, i);
+		if (!board[i])
+		{
+			ft_free(board, i);
+			free(board);
+			return (write(1, "Error\n", 6), 0);
+		}
+		i++;
+	}
+
+	int y;
+	int x = 0;
+	while (x < size)
+	{
+		y = 0;
+		while (y < size)
+		{
+			printf("%d ", board[x][y]);
+			y++;
+		}
+		printf("\n");
+		x++;
+	}
+	// ft_rush(board, size);
+	return (0);
 }
